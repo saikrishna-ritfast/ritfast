7f68ff20341995b534d9a0a47722fa47fdd99f0e
PITLA SAI KRISHNA <p.saikrishna@modlix.com>
2026-02-01 20:29:55 +0530

    Pagination adding

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,46 +2,53 @@ import styles from '../styles/table.module.css'
 import type { CommonTableProps } from '../utils/tableData'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSnowflake } from "@fortawesome/free-regular-svg-icons";
+import { Pagination } from 'antd';
 
 const CommonTable = <T,>({ columns, data }: CommonTableProps<T>) => {
     return (
-        <table className={styles.table}>
-            <thead className={styles.thead}>
-                <tr>
-                    {columns.map((col) => (
-                        <th key={String(col.key)} className={styles.th}>
-                            {col.header}
-                        </th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {data.length === 0 ? (
+        <>
+            <table className={styles.table}>
+                <thead className={styles.thead}>
                     <tr>
-                        <td colSpan={columns.length} className={styles.empty}>
-                            No data found
-                        </td>
+                        {columns.map((col) => (
+                            <th key={String(col.key)} className={styles.th}>
+                                {col.header}
+                            </th>
+                        ))}
                     </tr>
-                ) : (
-                    data.map((row, index) => (
-                        <tr key={index} className={styles.tr}>
-                            {columns.map((col) => (
-                                <td key={String(col.key)} className={styles.td}>
-                                    {/* {String(row[col.key])} */}
-                                    {col.key === "tag" ? (
-                                        <span className={row[col.key] === "Hot" ? styles.tagHot : row[col.key] === "Cold" ? styles.tagCold : styles.tagWarm}>{row[col.key] === "Hot" ? <i className="bi bi-fire" /> : row[col.key] === "Cold" ? <FontAwesomeIcon icon={faSnowflake} /> : <i className="bi bi-cup-hot-fill" />}{String(row[col.key])}</span>
-                                    ) : col.key === "status" ? (
-                                        <span className={row[col.key] === "Active" ? styles.tagActive : styles.tagInactive}>{String(row[col.key])}</span>
-                                    ) : (
-                                        String(row[col.key])
-                                    )}
-                                </td>
-                            ))}
+                </thead>
+                <tbody>
+                    {data.length === 0 ? (
+                        <tr>
+                            <td colSpan={columns.length} className={styles.empty}>
+                                No data found
+                            </td>
                         </tr>
-                    ))
-                )}
-            </tbody>
-        </table>
+                    ) : (
+                        data.map((row, index) => (
+                            <tr key={index} className={styles.tr}>
+                                {columns.map((col) => (
+                                    <td key={String(col.key)} className={styles.td}>
+                                        {/* {String(row[col.key])} */}
+                                        {col.key === "tag" ? (
+                                            <span className={row[col.key] === "Hot" ? styles.tagHot : row[col.key] === "Cold" ? styles.tagCold : styles.tagWarm}>{row[col.key] === "Hot" ? <i className="bi bi-fire" /> : row[col.key] === "Cold" ? <FontAwesomeIcon icon={faSnowflake} /> : <i className="bi bi-cup-hot-fill" />}{String(row[col.key])}</span>
+                                        ) : col.key === "status" ? (
+                                            <span className={row[col.key] === "Active" ? styles.tagActive : styles.tagInactive}>{String(row[col.key])}</span>
+                                        ) : (
+                                            String(row[col.key])
+                                        )}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))
+                    )}
+                </tbody>
+            </table>
+            <Pagination className={styles.pagination}
+                styles={{ item: { padding: '0px' } }}
+                total={data.length} showSizeChanger showQuickJumper showTotal={(total) => `Total ${total} items`}
+            />
+        </>
     );
 };
 
--- src/pages/Track/index.tsx
+++ src/pages/Track/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Layout from '../Layout'
+import { Pagination } from 'antd';
+
 
 const Track = () => {
     return (
